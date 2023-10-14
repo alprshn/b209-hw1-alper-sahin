0f37268f0d3747f058cc6c74ae728116c1673b3b
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-10-14 20:05:41 +0300

    Edit Calculator

--- b209-hw1-alper-sahin/Form1.cs
+++ b209-hw1-alper-sahin/Form1.cs
@@ -3,6 +3,8 @@ namespace b209_hw1_alper_sahin
     public partial class Form1 : Form
     {
         private char process;
+        private bool cleanScreen;
+        private int firstNumber;
 
         public Form1()
         {
@@ -21,60 +23,111 @@ namespace b209_hw1_alper_sahin
 
         private void numberOne_Click(object sender, EventArgs e)
         {
+            if (cleanScreen)
+            {
+                calculatorScreen.Text = "";
+                cleanScreen = false;
+            }
             if (calculatorScreen.Text == "0") calculatorScreen.Text = "";
             calculatorScreen.Text += "1";
         }
 
         private void numberTwo_Click(object sender, EventArgs e)
         {
+            if (cleanScreen)
+            {
+                calculatorScreen.Text = "";
+                cleanScreen = false;
+            }
             if (calculatorScreen.Text == "0") calculatorScreen.Text = "";
             calculatorScreen.Text += "2";
         }
 
         private void numberThree_Click(object sender, EventArgs e)
         {
+            if (cleanScreen)
+            {
+                calculatorScreen.Text = "";
+                cleanScreen = false;
+            }
             if (calculatorScreen.Text == "0") calculatorScreen.Text = "";
             calculatorScreen.Text += "3";
         }
 
         private void numberFour_Click(object sender, EventArgs e)
         {
+            if (cleanScreen)
+            {
+                calculatorScreen.Text = "";
+                cleanScreen = false;
+            }
             if (calculatorScreen.Text == "0") calculatorScreen.Text = "";
             calculatorScreen.Text += "4";
         }
 
         private void numberFive_Click(object sender, EventArgs e)
         {
+            if (cleanScreen)
+            {
+                calculatorScreen.Text = "";
+                cleanScreen = false;
+            }
             if (calculatorScreen.Text == "0") calculatorScreen.Text = "";
             calculatorScreen.Text += "5";
         }
 
         private void numberSix_Click(object sender, EventArgs e)
         {
+            if (cleanScreen)
+            {
+                calculatorScreen.Text = "";
+                cleanScreen = false;
+            }
             if (calculatorScreen.Text == "0") calculatorScreen.Text = "";
             calculatorScreen.Text += "6";
         }
 
         private void numberSeven_Click(object sender, EventArgs e)
         {
+            if (cleanScreen)
+            {
+                calculatorScreen.Text = "";
+                cleanScreen = false;
+            }
             if (calculatorScreen.Text == "0") calculatorScreen.Text = "";
             calculatorScreen.Text += "7";
         }
 
         private void numberEight_Click(object sender, EventArgs e)
         {
+            if (cleanScreen)
+            {
+                calculatorScreen.Text = "";
+                cleanScreen = false;
+            }
             if (calculatorScreen.Text == "0") calculatorScreen.Text = "";
             calculatorScreen.Text += "8";
         }
 
         private void numberNine_Click(object sender, EventArgs e)
         {
+            if (cleanScreen)
+            {
+                calculatorScreen.Text = "";
+                cleanScreen = false;
+            }
             if (calculatorScreen.Text == "0") calculatorScreen.Text = "";
             calculatorScreen.Text += "9";
         }
 
         private void numberZero_Click(object sender, EventArgs e)
         {
+            if (cleanScreen) 
+            { 
+                calculatorScreen.Text = "";
+                cleanScreen = false;
+            }
+                
             if (calculatorScreen.Text == "0") calculatorScreen.Text = "";
             calculatorScreen.Text += "0";
         }
@@ -82,6 +135,8 @@ namespace b209_hw1_alper_sahin
         private void additionButton_Click(object sender, EventArgs e)
         {
             process = '+';
+            cleanScreen = true;
+            firstNumber = Convert.ToInt32(calculatorScreen.Text);
         }
     }
 }
