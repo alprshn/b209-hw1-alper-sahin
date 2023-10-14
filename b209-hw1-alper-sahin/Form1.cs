1ddaa2cfecb874d27daf8dcfd1a863d3f2ffedb7
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-10-14 19:39:43 +0300

    Edit Calculator Design

---
+++ b209-hw1-alper-sahin/Form1.cs
@@ -0,0 +1,20 @@
+namespace b209_hw1_alper_sahin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
